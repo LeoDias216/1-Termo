b178123a856ec6c0dec12c4b50aaa163a4c16b48
Leonardo Dias <diasleohenri@gmail.com>
2026-05-08 10:02:20 -0300

    mudanças

--- Python/ATIVIDADES/cancelas.py
+++ Python/ATIVIDADES/cancelas.py
@@ -1,34 +1,52 @@
 import time
+
 vaga = 500
-while vaga > 0:
+
+while True:
     print("Bem vindo ao estacionamento do shopping!")
     print(f"Ainda temos {vaga} vagas disponíveis")
+
     print("Siga abaixo a tabela de preços:")
     print("Até 15 minutos: Grátis")
     print("Até 3 horas: Valor fixo de R$ 15,00.")
     print("Hora adicional: R$ 3,00 por hora extra")
     print("Obs: Clientes com TAG possuem 10% de desconto sobre o valor total, lembre de verificar se a TAG está ativa.")
     print("")
+
     print("Digite 1 se deseja ticket físico, 2 para TAG ou 3 para sair do estacionamento")
-    tipo_de_acesso = float(input("Insira a opção desejada: "))
+    tipo_de_acesso = int(input("Insira a opção desejada: "))
+
+# Entrada com o ticket:
     if tipo_de_acesso == 1:
-        hora_de_entrada_ticket = float(input("Insira o horário atual como o exemplo (14:30 = 1430 ou 17:00 = 1700): "))
-        print("Pegue seu ticket impreso e avance pela catraca aberta")
-        vaga -= 1
-        time.sleep(5)
+        if vaga > 0:
+            hora_de_entrada_ticket = int(input("Insira o horário atual (14:30 = 1430): "))
+            print("Pegue seu ticket impresso e avance pela catraca aberta")
+            vaga -= 1
+            time.sleep(3)
+        else:
+            print("Estacionamento lotado para tickets!")
+
+# Entrada com a TAG:
     elif tipo_de_acesso == 2:
-        hora_de_entrada_tag = float(input("Insira o horário atual como o exemplo (14:30 = 1430 ou 17:00 = 1700): "))
-        print("Avance pela catraca aberta")
-        vaga -= 1
-        time.sleep(5)
+        tag_ativa = input("A TAG está ativa? (s/n): ")
+        if tag_ativa == "s":
+            hora_de_entrada_tag = int(input("Insira o horário atual (14:30 = 1430): "))
+            print("TAG válida, avance pela catraca aberta")
+            vaga -= 1
+            time.sleep(3)
+        else:
+            print("TAG inválida ou inativa!")
+
+# Saída:
     elif tipo_de_acesso == 3:
         print("Você escolheu sair")
         print("1=ticket")
         print("2=TAG")
         print("3=Perdi meu ticket")
-        forma_de_entrada = float(input("Insira a forma de entrada: "))
+        forma_de_entrada = int(input("Insira a forma de entrada: "))
+
         if forma_de_entrada == 1:
-            hora_de_saida = float(input("Insira o horário atual como o exemplo (14:30 = 1430 ou 17:00 = 1700): "))
+            hora_de_saida = int(input("Insira o horário atual como o exemplo (14:30 = 1430 ou 17:00 = 1700): "))
             hora_total = hora_de_saida - hora_de_entrada_ticket
             hora_a_ser_paga = hora_total - 40
             if hora_a_ser_paga <= 15:
@@ -42,7 +60,7 @@ while vaga > 0:
                 vaga += 1
                 time.sleep(3)
             elif hora_a_ser_paga > 180:
-                horas_extras = float(input("Quantas horas extras você ficou, além de 3? "))
+                horas_extras = int(input("Quantas horas extras você ficou, além de 3? "))
                 valor_por_hora = horas_extras * 3
                 valor_final = valor_por_hora + 15
                 print(f"O valor do seu ticket é de R${valor_final},00, insira a forma de pagamento")
@@ -51,7 +69,7 @@ while vaga > 0:
                 vaga += 1
                 time.sleep(3)
         elif forma_de_entrada == 2:
-            hora_de_saida = float(input("Insira o horário atual como o exemplo (14:30 = 1430 ou 17:00 = 1700): "))
+            hora_de_saida = int(input("Insira o horário atual como o exemplo (14:30 = 1430 ou 17:00 = 1700): "))
             hora_total = hora_de_saida - hora_de_entrada_tag
             hora_a_ser_paga = hora_total - 40
             hora_final = hora_a_ser_paga * 0.90
@@ -66,7 +84,7 @@ while vaga > 0:
                 vaga += 1
                 time.sleep(3)
             elif hora_final > 180:
-                horas_extras = float(input("Quantas horas extras você ficou, além de 3? "))
+                horas_extras = int(input("Quantas horas extras você ficou, além de 3? "))
                 valor_por_hora = horas_extras * 3
                 valor_final = valor_por_hora + 15
                 print(f"O valor da sua TAG é de R${valor_final},00, insira a forma de pagamento")
@@ -81,3 +99,4 @@ while vaga > 0:
             print("Pagamento concluído, catraca aberta, boa viagem!")
             vaga += 1
             time.sleep(3)
+            
