732c71a373c989444eb4d4d35cb2a64b5c775d1c
Leonardo Dias <diasleohenri@gmail.com>
2026-05-08 11:15:38 -0300

    levantamento de requisitos - elevador

---
+++ Python/Projetos/elevador.py
@@ -0,0 +1,14 @@
+# Sistema de elevador de prédio
+# O prédio possui 10 andares sendo o térreo o andar 0. O elevador pode se mover para cima e para baixo, e tem a capacidade de transportar até 5 pessoas.
+# O elevador começa no andar 0 e pode ser chamado por qualquer pessoa em qualquer andar.
+# O elevador deve se mover para o andar onde a pessoa chamou, e depois para o andar destino da pessoa.
+# O elevador deve exibir mensagens indicando o andar atual, o número de pessoas no elevador, e as ações realizadas (subindo, descendo, parando). O programa deve continuar rodando até que o usuário decida encerrar.
+
+# Levantamento de Ideias:
+# 10 andares, sendo 0 o térreo
+# Capacidade de subir e descer
+# Capacidade máxima de 5 pessoas
+# Começa no 0 e pode ser chamado em qualquer andar
+# Andar até onde a pessoa está e até o destino escolhido
+# Mensagens indicando o andar atual, número de pessoas no elevador e ação atual
+# O elevador pode parar se a senha do técnico for inserida
