9475b489e83200ba1646433b153ad47316e7e5a3
Leonardo Dias <diasleohenri@gmail.com>
2026-05-15 10:20:09 -0300

    elevador

--- Python/Projetos/elevador.py
+++ Python/Projetos/elevador.py
@@ -9,6 +9,45 @@
 # Capacidade de subir e descer
 # Capacidade máxima de 5 pessoas
 # Começa no 0 e pode ser chamado em qualquer andar
-# Andar até onde a pessoa está e até o destino escolhido
+# Anda até onde a pessoa está e até o destino escolhido
 # Mensagens indicando o andar atual, número de pessoas no elevador e ação atual
 # O elevador pode parar se a senha do técnico for inserida
+
+import time
+
+andar_do_elevador = 0
+
+while True:
+    print("")
+    print("Bem-vindo ao elevador Magnum Up, onde deseja ir hoje?")
+    print(f"Andar atual: {andar_do_elevador}")
+    print("A música de hoje é jazz")
+    print("0 = Térreo")
+    print("Andares 1 - 10")
+    print("AVISO: Máximo de 5 pessoas no elevador")
+    print("")
+    andar = int(input("Digite o andar que deseja ir: "))
+
+    if andar == andar_do_elevador:
+        print("Você já está nesse andar")
+        time.sleep(3)
+    elif andar <= 10:
+        print("Fechando portas...")
+        time.sleep(3)
+        if andar > andar_do_elevador:
+            print("SUBINDO...")
+            time.sleep(4)
+        elif andar < andar_do_elevador:
+            print("DESCENDO...")
+            time.sleep(4)
+        andar_do_elevador = andar
+        print(f"Andar atual: {andar_do_elevador}")
+        time.sleep(3)
+        print("Abrindo portas...")
+        time.sleep(3)
+    elif andar == 11:
+        print("Encerrando sistemas...")
+        break
+    else:
+        print("Digite um andar válido")
+        time.sleep(3)
