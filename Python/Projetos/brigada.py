3714f1449c9441ae769998f059e0b646b298eb7d
Leonardo Dias <diasleohenri@gmail.com>
2026-05-15 11:24:55 -0300

    ideias do projeto - Brigada

---
+++ Python/Projetos/brigada.py
@@ -0,0 +1,20 @@
+# Desenvolva um programa em Python que gerencie o status de conformidade dos funcionários de uma empresa.
+# 1 - Cadastro de funcionários:
+# Armazene o nome, setor e o status dos treinamentos (NR-10, NR-35 e Brigada).
+# 2 - Verificação de EPI (NR-6):
+# O sistema deve receber o setor do funcionário.
+# Se o setor for "Elétrica", liste a obrigatoriedade de luvas de alta tensão e botas dielétricas.
+# Se o setor for "Trabalho em Altura", liste o cinturão de segurança e talabarte.
+# 3 - Alerta de reciclagem:
+# Crie uma função que receba o ano do último treinamento da Brigada de Incêndio.
+# Se o treinamento tiver mais de 2 anos, exiba a mensagem: "Treinamento Vencido! Encaminhar para reciclagem."
+# Caso contrário, exiba: "Treinamento Válido."
+# 4 - Relatório geral:
+# Exiba na tela um resumo com o total de funcionários cadastrados e quantos estão com treinamentos em dia.
+
+# Levantamento de ideias:
+# Armazenar nome, setor e status dos treinamentos dos funcionários
+# Se o setor for elétrica, listar obrigatoriedade de luvas de alta tensão e botas dielétricas
+# Se o setor for trabalho em altura, listar o cinturão de segurança e talabarte
+# Função que analisa o ano do último treinamento de brigada e pode constar como vencido, para encaminhar para reciclagem
+# Exibir um resumo com o total de funcionários cadastrados e quantos estão com os treinamentos em dia
